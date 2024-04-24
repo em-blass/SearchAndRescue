bbe93b8f24bdfe3ef40db07ba72ca9081a97ac03
Patrick Jirele <pjjirele@gmail.com>
2024-04-23 19:11:37 -0600

    updated drone control

--- drone_control.py
+++ drone_control.py
@@ -1,1 +1,21 @@
 ## https://tello.oneoffcoder.com/python-auto-flight.html
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+tello.takeoff()
+
+def make_square():
+    for i in range(2):
+        tello.move_forward(50)  # Moves forward 30cm
+        tello.rotate_counter_clockwise(90)  # rotate 90 degrees counterclockwise
+        tello.move_forward(50)  # Moves forward 50cm
+        tello.rotate_counter_clockwise(90)  # rotate 90 degrees counterclockwise
+
+
+make_square()
+tello.move_down(10)
+make_square()
+tello.move_down(10)
+make_square()
+tello.land()
